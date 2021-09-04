b1eb5f167beb62d94357f46d7b7cd19c89135ad8
Lennon Donaghy <donaghylennon@gmail.com>
2021-09-04 16:31:27 +0100

    Add check for ROM argument

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include "CPU.h"
 
+#include <iostream>
+
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Error: no ROM provided\n");
+        exit(1);
+    }
     CPU test(argv[1]);
     test.run();
     return 0;
