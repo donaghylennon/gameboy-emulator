091896352344ca19a10208dbc4090cfb0a34fcc2
Lennon Donaghy <donaghylennon@gmail.com>
2021-09-05 19:23:04 +0100

    Added methods to PPU to draw scanlines, and emulate PPU cycles

--- PPU.cpp
+++ PPU.cpp
@@ -1,6 +1,6 @@
 #include "PPU.h"
 
-PPU::PPU(Memory& memory) {
+PPU::PPU(Memory& memory) : memory(memory) {
     window = SDL_CreateWindow("GBEMU", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
             160*display_scale, 144*display_scale, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -14,5 +14,75 @@ PPU::~PPU() {
     SDL_DestroyWindow(window);
 }
 
+void PPU::run_cycle() {
+    for (int i = 0; i < 4; i++) {
+        if (current_state == OAM_SCAN) {
+            oam_scan_counter++;
+            if (oam_scan_counter == 80) {
+                oam_scan_counter = 0;
+                current_state = DRAWING;
+            }
+        } else if (current_state == DRAWING) {
+            if (drawing_counter == 0)
+                run_line();
+            drawing_counter++;
+            if (drawing_counter == 43) {
+                drawing_counter = 0;
+                current_state = HBLANK;
+            }
+        } else if (current_state == HBLANK) {
+            hblank_counter++;
+            if (hblank_counter == 51) {
+                hblank_counter = 0;
+                if (current_line == 143) {
+                    current_state = VBLANK;
+                    current_line = 0;
+                } else {
+                    current_state = OAM_SCAN;
+                }
+            }
+        } else if (current_state == VBLANK) {
+            vblank_counter--;
+            if (vblank_counter == 0)
+                current_state = OAM_SCAN;
+        }
+    }
+}
+
 void PPU::run_line() {
+    for (int i = 0; i < 20; i++) {
+        fetch_tile_row();
+    }
+    current_line++;
+}
+
+void PPU::fetch_tile_row() {
+    unsigned tilemap_start;
+    if (memory.read(0xFF40) & 0x8) {
+        tilemap_start = 0x9C00;
+    } else {
+        tilemap_start = 0x9800;
+    }
+    unsigned tilemap_x = ((memory.read(0xFF43) / 8) + fetcher_x) & 0x1F;
+    unsigned tilemap_y = (current_line + memory.read(0xFF42)) & 0xFF;
+
+    unsigned tilemap_index = (tilemap_y/8) * 32 + tilemap_x;
+    uint8_t tile_index = memory.read(tilemap_start + tilemap_index);
+
+    uint8_t tiledata_low, tiledata_high;
+    if (memory.read(0xFF40) & 0x10) {
+        tiledata_low = memory.read(0x8000 + tile_index);
+        tiledata_high = memory.read(0x8000 + tile_index + 1);
+    } else {
+        tiledata_low = memory.read(0x9000 + (int8_t)tile_index);
+        tiledata_high = memory.read(0x9000 + (int8_t)(tile_index + 1));
+    }
+
+    for (int i = 0; i < 8; i++) {
+        uint8_t mask = 1 << i;
+        unsigned colour_index = ((tiledata_high & mask) >> (i-1)) & ((tiledata_low & mask) >> i);
+        background_fifo.push(colours[colour_index]);
+    }
+
+    fetcher_x++;
 }
--- PPU.h
+++ PPU.h
@@ -6,18 +6,34 @@
 
 #include <queue>
 
+enum PpuState {
+    OAM_SCAN, DRAWING, HBLANK, VBLANK
+};
+
 class PPU {
 public:
     PPU(Memory& memory);
     ~PPU();
 
+    void run_cycle();
+
     void run_line();
+    void fetch_tile_row();
 private:
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
+    Memory& memory;
     unsigned display_scale = 3;
-    std::queue<int> background_fifo;
+    std::queue<uint32_t> background_fifo;
     std::queue<int> object_fifo;
     uint32_t colours[4] = { 0x0, 0x55555555, 0xBBBBBBBB, 0xFFFFFFFF };
+
+    PpuState current_state = OAM_SCAN;
+    unsigned oam_scan_counter = 80;
+    unsigned drawing_counter = 43;
+    unsigned hblank_counter = 51;
+    unsigned vblank_counter = 4560;
+    unsigned current_line = 0;
+    unsigned fetcher_x = 0;
 };
